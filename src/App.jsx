57b168bf7e45b663076bf8101ccd010fa19db974
HamZik <b1y9mn@gmail.com>
2023-04-17 15:15:54 +0900

    chore: 기본 세팅 및 컴포넌트 분리

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Layout/Header';
+import Toppings from './components/Toppings/Toppings';
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+      <main>
+        <Toppings />
+      </main>
+    </>
+  )
+}
+
+export default App
