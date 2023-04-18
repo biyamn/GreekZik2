5e00b6b7d0cfd39eb06d44015b20d53a89e41007
HamZik <b1y9mn@gmail.com>
2023-04-18 13:15:00 +0900

    feat: Header 컴포넌트 생성

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>GreekZik</h1>
+        <HeaderCartButton />
+      </header>
+    </>
+  );
+};
+
+export default Header;
