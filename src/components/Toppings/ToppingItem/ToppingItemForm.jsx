809b5695ffe0d11c7c3079bf0bfb49ef45526469
b1y9mn <b1y9mn@gmail.com>
2023-04-19 01:29:41 +0900

    feat: form태그와 폼내용 추가

--- src/components/Toppings/ToppingItem/ToppingItemForm.jsx
+++ src/components/Toppings/ToppingItem/ToppingItemForm.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Input from '../../UI/Input';
 
 const ToppingItemForm = () => {
   return (
-    <div>
-      
-    </div>
+    <form>
+      <Input 
+        label="수량" 
+        input={{
+          id: 1,
+          type: 'number',
+          min: '1',
+          max: '10',
+          step: '1',
+          defaultValue: '1',
+        }} 
+      />
+      <button>+ 담기</button>
+    </form>
   );
 };
 
