7e32599e8dc335b0125b30457326de490c515a4c
b1y9mn <b1y9mn@gmail.com>
2023-04-19 01:29:11 +0900

    feat: ToppingItem에 제목, 가격, ToppingItemForm 컴포넌트 추가

--- src/components/Toppings/ToppingItem/ToppingItem.jsx
+++ src/components/Toppings/ToppingItem/ToppingItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import ToppingItemForm from './ToppingItemForm';
 
 const ToppingItem = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <h3>무화과</h3>
+        <div>1,000원</div>
+      </div>
+      <div>
+        <ToppingItemForm />
+      </div>
+    </>
   );
 };
 
