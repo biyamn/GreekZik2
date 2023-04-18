332f302be1dee13fe35990527b462b0faf16dde7
HamZik <b1y9mn@gmail.com>
2023-04-18 15:11:45 +0900

    chore: 기본 파일 세팅

---
+++ src/components/Toppings/ToppingItem/ToppingItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToppingItem = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default ToppingItem;
---
+++ src/components/Toppings/ToppingItem/ToppingItemForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToppingItemForm = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default ToppingItemForm;
