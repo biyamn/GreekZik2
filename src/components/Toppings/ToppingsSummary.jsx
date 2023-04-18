056bb322f9266d7e26d58f0eb3b1ea115def2c42
HamZik <b1y9mn@gmail.com>
2023-04-18 15:11:16 +0900

    feat: ToppingsSummary 컴포넌트 생성

---
+++ src/components/Toppings/ToppingsSummary.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../UI/Card';
+
+const ToppingsSummary = () => {
+  return (
+    <section>
+      <Card>
+        <p>최상급의 우유로 만든 꾸덕한 그릭요거트, 최고급 토핑을 제공하는 'GreekZik'입니다.</p>
+        <p>완벽히 커스텀 된 당신만의 그릭요거트를 즐기세요!</p>
+      </Card>
+    </section>
+  );
+};
+
+export default ToppingsSummary;
