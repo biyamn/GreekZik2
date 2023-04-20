2f63181e2e9d4681766a1714da516dd93f1a2031
b1y9mn <b1y9mn@gmail.com>
2023-04-20 14:53:11 +0900

    design: css 수정

--- src/components/Toppings/Toppings.jsx
+++ src/components/Toppings/Toppings.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ToppingsSummary from './ToppingsSummary';
 import AvailableToppings from './AvailableToppings';
+import classes from './Toppings.module.css';
 
 const Toppings = () => {
   return (
-    <>
+    <div className={classes.toppings}>
       <ToppingsSummary />
       <AvailableToppings />
-    </>
+    </div>
   );
 };
 
