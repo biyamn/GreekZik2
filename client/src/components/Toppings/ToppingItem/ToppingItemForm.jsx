3d7e86b6160f0f6534b23bf8bab108494c987d4b
b1y9mn <b1y9mn@gmail.com>
2023-05-02 10:43:35 +0900

    feat: onChangeHandler 추가

--- client/src/components/Toppings/ToppingItem/ToppingItemForm.jsx
+++ client/src/components/Toppings/ToppingItem/ToppingItemForm.jsx
@@ -3,15 +3,28 @@ import Input from '../../UI/Input';
 import classes from './ToppingItemForm.module.css';
 
 const ToppingItemForm = props => {
-  const [item, setItem] = useState({
+
+  console.log('selectedTopping: ', props.topping) // 선택한 카테고리의 토핑들이 잘 나옴
+  // 초기에 설정한 카테고리의 토핑들만 나옴
+  const [itemState, setItemState] = useState({
     id: props.topping.id,
     name: props.topping.name,
     description: props.topping.description,
     price: props.topping.price,
     amount: props.topping.amount
   });
+
+  console.log('selectedToppings: ', props.topping);
+  // let item = {
+  //   id: props.topping.id,
+  //   name: props.topping.name,
+  //   description: props.topping.description,
+  //   price: props.topping.price,
+  //   amount: props.topping.amount
+  // };
+
   
-  console.log('item: ', item)
+  console.log('itemState: ', itemState)
   // 8 1 1 1 1 1 9 1(선택된 카테고리의 amount들을 가져옴)
   // console.log('props.amount: ', props.amount)
   // 1 1 2 1 1 1 3 1(선택되는 거랑 상관없이 초기값으로 설정된 카테고리의 amount들을 가져옴)
@@ -21,23 +34,23 @@ const ToppingItemForm = props => {
     e.preventDefault();
     const enteredAmount = Number(item.amount);
     // console.log('form에서 받은 수량: ', enteredAmount)
-    setItem({
-      ...item,
-      amount: enteredAmount
-    }); // 여기서 onSaveItem을 호출했기 때문에 ToppingItem에서 onSaveItem이 실행되고 selectedItemData 객체가 만들어짐
+    item.amount = enteredAmount; // 여기서 onSaveItem을 호출했기 때문에 ToppingItem에서 onSaveItem이 실행되고 selectedItemData 객체가 만들어짐
+  }
+
+  const onChangeHandler = (e) => {
+    setItemState(prev => {
+      return {
+      ...prev,
+      amount: e.target.value,
+      }
+    })
   }
 
   // useEffect(() => console.log('amount: ', item.amount));
   return (
     <form className={classes.form} onSubmit={submitHandler} key={props.id} >
       <Input 
-        onChange={e => {
-          setItem({
-            ...item,
-            amount: e.target.value
-          })
-          
-        }}
+        onChange={onChangeHandler}
         label="수량" 
         input={{
           id: props.id,
@@ -45,8 +58,8 @@ const ToppingItemForm = props => {
           min: '1',
           max: '10',
           step: '1',
-          // value: item.amount,
-          value: props.amount
+          value: itemState.amount,
+          // value: props.amount
         }} 
       />
       <button type="submit">+ 담기</button>
