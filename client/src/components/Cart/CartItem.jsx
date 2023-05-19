058f0da2122749d80ee226a55a452890291167f4
b1y9mn <b1y9mn@gmail.com>
2023-05-19 09:34:59 +0900

    feat: prop에서 onAdd => handleAddItem

--- client/src/components/Cart/CartItem.jsx
+++ client/src/components/Cart/CartItem.jsx
@@ -1,9 +1,6 @@
 import classes from './CartItem.module.css';
 
-const CartItem = ({ price, name, amount, onRemove, onAdd, id }) => {
-  // n개의 수량을 선택한 후 담기 버튼을 누르면 n만큼의 수량이 CartItem에 찍혀있어야 함(amount)
-  // 그니까 ToppingItemForm의 수량과 동기화하라는 것이 아님. 그저 'submit 되었을 때' 그 수량이 찍히면 됨.
-  // 그럼 amount가 state여야겠네? 여러개여서 공간도 여러개여야겠고? 
+const CartItem = ({ price, name, amount, handleAddItem, handleRemoveItem, id }) => {
   return (
     <li className={classes['cart-item']}>
       <div>
