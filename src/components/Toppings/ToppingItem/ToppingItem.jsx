7a5c09994c884944ba3bbcdc8e236f792b95c7d0
b1y9mn <b1y9mn@gmail.com>
2023-05-26 13:12:56 +0900

    feat: toppingItem에 이미지 삽입

--- src/components/Toppings/ToppingItem/ToppingItem.jsx
+++ src/components/Toppings/ToppingItem/ToppingItem.jsx
@@ -1,28 +1,27 @@
 import React, { useState } from 'react';
 import ToppingItemForm from './ToppingItemForm';
 import classes from './ToppingItem.module.css';
-
+import yogurtImage from '../../../assets/yogurt.jpg';
 const ToppingItem = props => {
   const { price, key, name, description, id, amount, topping, backendData } = props;
   const commaSeparatedPrice = price.toLocaleString(3);
 
   return (
     <li className={classes.container} key={key}>
-      <div className={classes.namePriceDescription}>
         <h3 className={classes.name}>{name}</h3>
-        <div className={classes.price}>{`${commaSeparatedPrice}원`}</div>
+        <img className={classes.image} src={yogurtImage} alt={topping.name} />
         <div className={classes.description}>{description}</div>
-      </div>
-      <div className={classes.form}>
-        <ToppingItemForm
-          id={id}  
-          amount={amount} 
-          onSaveItem={props.onSaveItem} 
-          topping={topping}
-          backendData={backendData}
-          onSaveCategories={props.onSaveCategories}
-        />
-      </div>
+        <div className={classes.form}>
+          <ToppingItemForm
+            id={id}  
+            amount={amount} 
+            onSaveItem={props.onSaveItem} 
+            topping={topping}
+            backendData={backendData}
+            onSaveCategories={props.onSaveCategories}
+          />
+        </div>
+        <div className={classes.price}>{`${commaSeparatedPrice}원`}</div>
     </li>
   );
 };
