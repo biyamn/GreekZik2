a35d6b902fd3a147f164b4b8e9a467c399d6ce27
biyam <101965666+biyamn@users.noreply.github.com>
2023-08-10 02:14:32 +0900

    Revert "Merge pull request #24 from biyamn/feature/layout-mainpage"
    This reverts commit 2bf496ba30ca38071abffd28d14d51efb484f723.

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,88 @@
-import MainCartModal from "./components/Cart/MainCartModal";
-import React, { useState, useEffect } from "react";
-import MainHeader from "./components/Layout/MainHeader";
-import CartProvider from "./store/CartProvider";
-import SelectTopping from "./components/SelectTopping/SelectTopping";
-import SubCartModal from "./components/Cart/SubCartModal";
-import "./App.css";
-import NavigationBar from "./components/Layout/NavigationBar";
-import CartLayout from "./components/Layout/CartLayout";
-import MenuLayout from "./components/Layout/MenuLayout";
-import styled from "styled-components";
+import MainCartModal from './components/Cart/MainCartModal';
+import React, { useState, useEffect } from 'react';
+import MainHeader from './components/Layout/MainHeader';
+import CartProvider from './store/CartProvider';
+import SelectTopping from './components/SelectTopping/SelectTopping';
+import SubCartModal from './components/Cart/SubCartModal';
+import './App.css';
 
 function App() {
   const [backendData, setBackendData] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("ca2");
+  const [selectedCategory, setSelectedCategory] = useState('ca2');
   const [mainCartIsShown, setMainCartIsShown] = useState(false);
   const [subCartIsShown, setSubCartIsShown] = useState(false);
   // const [isLoading, setIsLoading] = useState(false);
-  const [status, setStatus] = useState("init");
+  const [status, setStatus] = useState('init');
   const [selectModalIsShown, setSelectModalIsShown] = useState(false);
 
   const fetchData = async () => {
-    setStatus("loading");
+    setStatus('loading');
 
     try {
-      const response = await fetch(
-        "https://greek-yogurt-order-app-17351-default-rtdb.firebaseio.com/data.json"
-      );
-      const data = await response.json();
-      // setIsLoading을 같은 핸들러에서 써도 되는 이유: await로 시간차가 생기기 때문에
-
-      setStatus("loaded");
-      setBackendData(data);
-    } catch (error) {
-      console.log("error: ", error);
+    const response = await fetch('https://greek-yogurt-order-app-17351-default-rtdb.firebaseio.com/data.json');
+    const data = await response.json();
+    // setIsLoading을 같은 핸들러에서 써도 되는 이유: await로 시간차가 생기기 때문에
+    
+    setStatus('loaded');
+    setBackendData(data);
+    } catch(error) {
+      console.log('error: ', error)
     }
-  };
+  }
 
   useEffect(() => {
     // console.log('useEffect 시작')
     fetchData();
-  }, []);
-
+  }, [])
+  
   const showMainCartHandler = () => {
     setMainCartIsShown(true);
-  };
+  }
 
   const hideMainCartHandler = () => {
     setMainCartIsShown(false);
-  };
+  }
 
   const showSubCartHandler = () => {
     setSubCartIsShown(true);
-  };
+  }
 
   const hideSubCartHandler = () => {
     setSubCartIsShown(false);
-  };
+  }
 
   const showSelectModalHandler = () => {
     setSelectModalIsShown(true);
-  };
+  }
 
   const hideSelectModalHandler = () => {
     setSelectModalIsShown(false);
-  };
+  }
 
-  if (status === "loading" || status === "init") {
-    return <div>Loading...</div>;
-  } else if (status === "loaded") {
+
+  if (status === 'loading' || status === 'init') {
+    return <div>Loading...</div>
+  } else if (status === 'loaded') {
     return (
       <CartProvider>
-        {mainCartIsShown && (
-          <MainCartModal hideMainCartHandler={hideMainCartHandler} />
-        )}
-        {/* <MainHeader showMainCartHandler={showMainCartHandler} /> */}
-        <Main>
-          {subCartIsShown && (
-            <SubCartModal hideSubCartHandler={hideSubCartHandler} />
-          )}
-          <NavigationBar />
-          <MenuLayout
+        {mainCartIsShown &&<MainCartModal hideMainCartHandler={hideMainCartHandler} />}
+        <MainHeader showMainCartHandler={showMainCartHandler} />
+        <main>
+        {subCartIsShown &&<SubCartModal hideSubCartHandler={hideSubCartHandler} />}
+          <SelectTopping
             selectModalIsShown={selectModalIsShown}
             showSelectModalHandler={showSelectModalHandler}
-            hideSelectModalHandler={hideSelectModalHandler}
+            hideSelectModalHandler={hideSelectModalHandler} 
             showSubCartHandler={showSubCartHandler}
-            backendData={backendData}
-            setBackendData={setBackendData}
-            selectedCategory={selectedCategory}
+            backendData={backendData} 
+            setBackendData={setBackendData} 
+            selectedCategory={selectedCategory}  
             setSelectedCategory={setSelectedCategory}
           />
-          <CartLayout />
-        </Main>
+        </main>
       </CartProvider>
     );
   }
 }
-
-const Main = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
 
 export default App;
--- src/components/SelectTopping/SelectTopping.jsx
+++ src/components/SelectTopping/SelectTopping.jsx
@@ -1,55 +1,45 @@
-import React, { useState } from "react";
-import SelectToppingModal from "./SelectToppingModal";
-import ToppingsSummary from "../Toppings/ToppingsSummary";
-import CartContext from "../../store/cartContext";
-import { useContext } from "react";
+import React, { useState } from 'react';
+import SelectToppingModal from './SelectToppingModal';
+import ToppingsSummary from '../Toppings/ToppingsSummary';
+import CartContext from '../../store/cartContext';
+import { useContext } from 'react';
 
-const Select = ({
-  showSelectModalHandler,
-  hideSelectModalHandler,
-  showSubCartHandler,
-  backendData,
-  setBackendData,
-  selectedCategory,
-  setSelectedCategory,
-  selectModalIsShown,
-}) => {
+const Select = ({ showSelectModalHandler, hideSelectModalHandler, showSubCartHandler, backendData, setBackendData, selectedCategory, setSelectedCategory, selectModalIsShown }) => {
   // console.log('왜')
   const { onSave } = useContext(CartContext);
 
-  const handleClickYogurt = (selectedItemData) => {
+  const handleClickYogurt = selectedItemData => {
     onSave(selectedItemData);
     showSelectModalHandler();
-  };
+  }
 
   return (
     <>
-      {/* <ToppingsSummary /> */}
+      <ToppingsSummary />
       <ul>
         {backendData[0].yogurtData[0].DUMMY_TOPPINGS.map((yogurt) => {
           return (
             <li key={yogurt.id}>
               <p>{yogurt.name}</p>
               <p>{yogurt.price}원</p>
-              <button onClick={() => handleClickYogurt(yogurt)}>
-                한그릇 만들기 시작!
-              </button>
+              <button onClick={() => handleClickYogurt(yogurt)}>한그릇 만들기 시작!</button>
             </li>
-          );
-        })}
+          )})
+        }
       </ul>
-      {selectModalIsShown && (
-        <SelectToppingModal
-          showSubCartHandler={showSubCartHandler}
-          showSelectModalHandler={showSelectModalHandler}
-          hideSelectModalHandler={hideSelectModalHandler}
-          backendData={backendData}
-          setBackendData={setBackendData}
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
-      )}
+      {selectModalIsShown && <SelectToppingModal 
+        showSubCartHandler={showSubCartHandler}
+
+        showSelectModalHandler={showSelectModalHandler}
+        hideSelectModalHandler={hideSelectModalHandler}
+
+        backendData={backendData} 
+        setBackendData={setBackendData} 
+        selectedCategory={selectedCategory}  
+        setSelectedCategory={setSelectedCategory} 
+      />}
     </>
+    
   );
 };
 
